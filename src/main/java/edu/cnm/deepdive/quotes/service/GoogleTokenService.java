9b0dad7a9d21c7bc9aee30ca6365821a0da96021
Austin Preston <austinpreston314@gmail.com>
2020-07-13 11:59:34 -0600

    Almost done with Google OAuth 2.0 integration

--- src/main/java/edu/cnm/deepdive/quotes/service/GoogleTokenService.java
+++ src/main/java/edu/cnm/deepdive/quotes/service/GoogleTokenService.java
@@ -6,16 +6,22 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import edu.cnm.deepdive.quotes.model.entity.User;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Collection;
 import java.util.Collections;
-import net.bytebuddy.utility.JavaConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.exceptions.InvalidTokenException;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
@@ -30,11 +36,13 @@ public class GoogleTokenService implements ResourceServerTokenServices {
 
   private final String clientId;
   private final AccessTokenConverter converter;
-  // TODO Reference UserService.
+  private final UserService userService;
 
   @Autowired
-  public GoogleTokenService(@Value("${oauth.clientId}") String clientId) {
+  public GoogleTokenService(@Value("${oauth.clientId}") String clientId,
+      UserService userService) {
     this.clientId = clientId;
+    this.userService = userService;
     converter = new DefaultAccessTokenConverter();
   }
 
@@ -57,9 +65,13 @@ public class GoogleTokenService implements ResourceServerTokenServices {
       Payload payload = idToken.getPayload();
       String oauthKey = payload.getSubject();
       String displayName = (String) payload.get("name");
-      // TODO Extra any additioanl required info from payload
-      // TODO Request user from UserService.
-      return null; // TODO Replace with actual value.
+      // TODO Extra any additional required info from payload
+      User user = userService.readOrCreateOne(oauthKey, displayName);
+      Collection<GrantedAuthority> grants = Collections.singleton(
+          new SimpleGrantedAuthority(String.format(ROLE_PATTERN, user.getRole())));
+      Authentication base = new UsernamePasswordAuthenticationToken(user, token, grants);
+      OAuth2Request request = converter.extractAuthentication(payload).getOAuth2Request();
+      return new OAuth2Authentication(request, base);
     } else {
       throw new InvalidTokenException(VERIFICATION_FAILURE);
     }
